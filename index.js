b779b0dc773230f2ca8f616ba6491e91b50a4c0a
Juan Rodriguez <juan.rodriguez@201949-MACBOOK.local>
2024-11-13 19:57:33 -0500

    Completed polygon api call with data

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-
+import { dates } from './data/dates.js';
 
 const tickersArr = []
 
-// const generateReportBtn = document.querySelector('.generate-report-btn')
-
-// generateReportBtn.addEventListener('click', fetchStockData)
-
-// document.getElementById('ticker-input-form').addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     const tickerInput = document.getElementById('ticker-input')
-//     if (tickerInput.value.length > 2) {
-//         generateReportBtn.disabled = false
-//         const newTickerStr = tickerInput.value
-//         tickersArr.push(newTickerStr.toUpperCase())
-//         tickerInput.value = ''
-//         renderTickers()
-//     } else {
-//         const label = document.getElementsByTagName('label')[0]
-//         label.style.color = 'red'
-//         label.textContent = 'You must add at least one ticker. A ticker is a 3 letter or more code for a stock. E.g TSLA for Tesla.'
-//     }
-// })
-
-let sp500Companies = [];
-let currentIndex = -1;
-const textInput = document.getElementById('company-input')
+let sp500Companies = []
+let currentIndex = -1
+let foundCompany = false
+const searchBar = document.getElementById('company-input')
 const suggestionsDiv = document.getElementById('suggestions')
+const addTickerButton = document.getElementById('add-ticker-btn')
+const errorlabel = document.querySelector('.error-input')
+const notFoundLabel = document.querySelector('.not-found')
+const tickersContainer = document.getElementById('tickers-container')
+const generateReportBtn = document.querySelector('.generate-report-btn')
 
-textInput.addEventListener('input', handleCompanySearch)
-textInput.addEventListener('keydown', handleKeyNavigation);
+
+searchBar.addEventListener('input', handleCompanySearch)
+searchBar.addEventListener('keydown', handleKeyNavigation)
+addTickerButton.addEventListener('click', addTicker)
+generateReportBtn.addEventListener('click', fetchStockData)
+
 
 
 async function loadAndSetCompanies() {
@@ -38,40 +28,46 @@ async function loadAndSetCompanies() {
       throw new Error('Network response was not ok')
     }
     sp500Companies = await response.json()
-    console.log('Companies loaded:', sp500Companies)
   } catch (error) {
     console.error('Error loading JSON:', error)
   }
 }
-loadAndSetCompanies();
-
+loadAndSetCompanies()
 
 
 function handleCompanySearch(event) {
-  const input = event.target.value.toLowerCase()
-  suggestionsDiv.innerHTML = ''
+  const input = event.target.value.toLowerCase();
+  suggestionsDiv.innerHTML = '';
 
   if (input.length > 0) {
-    const matches = sp500Companies.filter(company => company.name.toLowerCase().includes(input) || company.ticker.toLowerCase().includes(input)).slice(0, 5)
-    if (matches.length > 1) {
-      matches.forEach(match => {
-        const div = document.createElement('div')
-        div.textContent = `${match.ticker} - ${match.name}`
-        div.classList.add('suggestion-item')
-        div.addEventListener('click', () => {
-          document.getElementById('company-input').value = `${match.ticker} - ${match.name}`
-          suggestionsDiv.innerHTML = ''
-        });
-        suggestionsDiv.appendChild(div)
-      })
-    }else {
-      const notFoundDiv = document.createElement('div')
-      notFoundDiv.textContent = 'Your search is not a company in the S&P 500'
-      notFoundDiv.classList.add('not-found')
-      suggestionsDiv.appendChild(notFoundDiv)
-    }  
-  } 
+    const matches = sp500Companies.filter(company => company.name.toLowerCase().includes(input) || company.ticker.toLowerCase().includes(input)).slice(0, 7)
+
+    if (matches.length > 0) {
+      notFoundLabel.classList.remove('active')
+      showSuggestions(matches)
+    } else {
+      notFoundLabel.classList.add('active')
+    }
+  } else {
+    notFoundLabel.classList.remove('active')
+  }
 }
+
+function showSuggestions(matches) {
+
+  matches.forEach(match => {
+    const div = document.createElement('div')
+    div.textContent = `${match.ticker} - ${match.name}`
+    div.classList.add('suggestion-item')
+    div.addEventListener('click', () => {
+      document.getElementById('company-input').value = `${match.ticker}`
+      suggestionsDiv.innerHTML = ''
+      foundCompany = true
+    });
+    suggestionsDiv.appendChild(div)
+  });
+}
+
 
 function handleKeyNavigation(event) {
   const items = document.querySelectorAll('.suggestion-item')
@@ -104,43 +100,85 @@ function highlightItem(items) {
 }
 
 
+function addTicker(event) {
+  console.log("add ticker")
 
+  event.preventDefault()
+    if (foundCompany) {
+      const newTickerStr = searchBar.value
+      tickersArr.push(newTickerStr.toUpperCase())
+      createTickerElement(newTickerStr)
+      searchBar.value = ''
+      foundCompany = false
+      renderTickers()
+      updateButtonStates();
+    } 
+}
+
+function createTickerElement(tickerText) {
+  const tickerContainer = document.createElement('div');
+  tickerContainer.classList.add('ticker-container');
+  const textSection = document.createElement('div');
+  textSection.classList.add('text-section');
+  textSection.textContent = tickerText;
+  
+  const closeButton = document.createElement('div');
+  closeButton.classList.add('close-button');
+
+  closeButton.addEventListener('click', () => {
+    removeTicker(tickerText);
+  });
+  
+  tickerContainer.appendChild(textSection);
+  tickerContainer.appendChild(closeButton);
+  tickersContainer.appendChild(tickerContainer);
+}
+
+function removeTicker(tickerText) {
+  tickersArr.splice(tickersArr.indexOf(tickerText), 1);
+  renderTickers();
+  updateButtonStates(); 
+}
 
 function renderTickers() {
-    const tickersDiv = document.querySelector('.ticker-choice-display')
-    tickersDiv.innerHTML = ''
-    tickersArr.forEach((ticker) => {
-        const newTickerSpan = document.createElement('span')
-        newTickerSpan.textContent = ticker
-        newTickerSpan.classList.add('ticker')
-        tickersDiv.appendChild(newTickerSpan)
-    })
+  tickersContainer.innerHTML = '';
+  tickersArr.forEach(createTickerElement);
 }
+
+function updateButtonStates() {
+  generateReportBtn.disabled = tickersArr.length === 0;
+  addTickerButton.disabled = tickersArr.length >= 3;
+}
+
 
 const loadingArea = document.querySelector('.loading-panel')
 const apiMessage = document.getElementById('api-message')
 
 async function fetchStockData() {
-    document.querySelector('.action-panel').style.display = 'none'
-    loadingArea.style.display = 'flex'
-    try {
-        const stockData = await Promise.all(tickersArr.map(async (ticker) => {
-            const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${process.env.POLYGON_API_KEY}`
-            const response = await fetch(url)
-            const data = await response.text()
-            const status = await response.status
-            if (status === 200) {
-                apiMessage.innerText = 'Creating report...'
-                return data
-            } else {
-                loadingArea.innerText = 'There was an error fetching stock data.'
-            }
-        }))
-        fetchReport(stockData.join(''))
-    } catch(err) {
-        loadingArea.innerText = 'There was an error fetching stock data.'
-        console.error('error: ', err)
-    }
+  console.log('Generating report...');
+  // document.querySelector('.action-panel').style.display = 'none';
+  loadingArea.style.display = 'flex';
+
+  try {
+      const stockData = await Promise.all(tickersArr.map(async (ticker) => {
+          const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=MATfx0pg_Pm0tLmMHI3aI1pPRa47VBpD`;
+          const response = await fetch(url);
+
+          if (!response.ok) {
+              throw new Error(`Error fetching data for ${ticker}: ${response.status} ${response.statusText}`);
+          }
+
+          const data = await response.json();
+          apiMessage.innerText = 'Creating report...';
+          return data;
+      }));
+
+      console.log('Stock data fetched successfully:', stockData);
+      // fetchReport(stockData.join('')); // Use this as needed
+  } catch (err) {
+      loadingArea.innerText = 'There was an error fetching stock data.';
+      console.error('Error:', err);
+  }
 }
 
 async function fetchReport(data) {
